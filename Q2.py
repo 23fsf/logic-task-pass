a107630db81c411197a7e4d6b436810009ab4a1c
Ahmed Rashid <110108794+23fsf@users.noreply.github.com>
2022-08-15 18:42:09 +0300

    Delete Q2.py

--- Q2.py
+++
@@ -1,7 +0,0 @@
-num=int(input("Enter number :"))
-for n in range(2,num):
-    if (num % n)==0:
-        print("the given number (",num,") is not prime number.")
-        break
-else:
-    print("the given number (",num,") is prime number.")
