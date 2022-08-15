ef8f76e51f25a50e699cbc2bb463ddf824b6f78a
Ahmed Rashid <110108794+23fsf@users.noreply.github.com>
2022-08-15 18:41:59 +0300

    Delete Q1.py

--- Q1.py
+++
@@ -1,9 +0,0 @@
-stry=input("Enter word: ")
-char_remove=input("Enter the char that you want to dalete :")
-stry2=""
-for i in stry:
-    if i==char_remove:
-        continue
-    else:
-        stry2+=i
-print(stry2)
